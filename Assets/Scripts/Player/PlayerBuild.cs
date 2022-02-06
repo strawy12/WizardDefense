07b0a91835bbea3b13a6aa19fd90001ec18d1197
suan9340 <sue9340@gmail.com>
2022-02-06 14:57:38 +0900

    설치 될떄 y 포지션 이상해

---
+++ Assets/Scripts/Player/PlayerBuild.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBuild : MonoBehaviour
+{
+    private RaycastHit hitTowerAreaInfo;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject towerObj;
+    [SerializeField] private GameObject towerPrefab;
+    [SerializeField] private Material[] towerMaterial;
+
+    private bool isClickF = false;
+    private bool isBuild;
+
+    private void Update()
+    {
+        BuildTower();
+        CanBuild(cam);
+    }
+
+    private void CanBuild(Camera _cam)
+    {
+        if (Physics.Raycast(_cam.transform.position, _cam.transform.forward, out hitTowerAreaInfo, maxDistance))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (isClickF)
+                {
+                    isClickF = false;
+                    towerObj.SetActive(false);
+                }
+            }
+
+            if (hitTowerAreaInfo.transform.gameObject.CompareTag("floor"))
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    towerObj.transform.position = new Vector3(hitTowerAreaInfo.point.x, hitTowerAreaInfo.point.y, hitTowerAreaInfo.point.z);
+                    Debug.Log("f 누름");
+                    isClickF = true;
+                    towerObj.SetActive(true);
+                }
+
+                if (isClickF && isBuild == false)
+                {
+                    towerObj.transform.position = hitTowerAreaInfo.point;
+                }
+
+            }
+        }
+    }
+
+    private void BuildTower()
+    {
+        if (isClickF)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (isBuild) return;
+                isClickF = false;
+                isBuild = true;
+                Debug.Log("타워설치요");
+                towerObj.SetActive(false);
+                Instantiate(towerPrefab, new Vector3(towerObj.transform.position.x, towerObj.transform.position.y, towerObj.transform.position.z), Quaternion.identity);
+                Debug.Log(towerObj.transform.position);
+                isBuild = false;
+            }
+        }
+    }
+
+}
